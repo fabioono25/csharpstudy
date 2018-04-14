fb015a8f26f6a04467c6a5fd26554ea8e454b057
ono <fabioono25@gmail.com>
2018-04-14 17:02:54 -0300

    hellow world

---
+++ CSharpStudy/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CSharpStudy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
