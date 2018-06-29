16cf5187a0fe3c86c5ad9df65012c58c1e8538a2
Fabio Tavoloni <fabio.t0844.beyond@xpi.com.br>
2018-06-29 09:35:30 -0300

    trabalhando com tuplas

--- CSharpStudy/Program.cs
+++ CSharpStudy/Program.cs
@@ -521,11 +521,39 @@ namespace CSharpStudy
             
 
             //Asynchronous Programming with Async/Await
-            DownloadHtmlAsync("http://www.google.com");
+            //DownloadHtmlAsync("http://www.google.com");
             //DownloadAsync("http://www.google.com");
-            var t = GetHtmlAsync("http://www.google.com");
+            //var t = GetHtmlAsync("http://www.google.com");
 
             #endregion
+        
+            #region C# 7
+
+                //TUPLAS
+                (int, int, int, string) parvalores = (1,2,3,"item 1");
+                Console.WriteLine(parvalores.Item1);
+                Console.WriteLine(parvalores.Item4);
+
+                var coord = (eixoX: 10, eixoY: 14);
+                Console.WriteLine($"Eixo X: {coord.eixoX}.");
+                Console.WriteLine($"Eixo Y: {coord.eixoY}.");
+
+                var calculados = CalcularValores(2);
+                Console.WriteLine($"Valor 2 x 2 = {calculados.vezesDois}");
+                Console.WriteLine($"Valor 2 elevado a 2 = {calculados.elevadoDois}");
+                Console.WriteLine($"Valor 2 mais 20 = {calculados.maisVinte}");
+
+                //tuple desconstruction example
+                (double multiplicado, double elevado, double vinte) = CalcularValores(10);
+                Console.WriteLine($"Valor 2 x 2 = {multiplicado}");
+                Console.WriteLine($"Valor 2 elevado a 2 = {elevado}");
+                Console.WriteLine($"Valor 2 mais 20 = {vinte}");                
+
+            #endregion
+        }
+
+        public static (double vezesDois, double elevadoDois, double maisVinte) CalcularValores(int valor){
+            return (valor * 2, System.Math.Pow(valor, 2), valor + 20);
         }
 
         #region C# Advanced
