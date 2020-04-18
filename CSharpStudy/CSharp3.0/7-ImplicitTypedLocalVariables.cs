e2e2f5fd8e9c2b915f9012a7ee1666c36b168716
Fabio Ono Tavoloni <fabioono25@gmail.com>
2020-04-18 15:38:50 +0100

    implicit typed var + partial methods

--- CSharpStudy/CSharp3.0/7-ImplicitTypedLocalVariables.cs
+++ CSharpStudy/CSharp3.0/7-ImplicitTypedLocalVariables.cs
@@ -4,7 +4,14 @@ namespace CSharpStudy.CSharp3_0
     {
         public static void ExecuteExample()
         {
-            
+            var number = 10; //implicit typed
+            int number2 = 10;//explicit typed
+
+            var x = new ClassTest(); //x is an instance of ClassTest
         }        
+    }
+
+    internal class ClassTest {
+
     }
 }
---
+++ CSharpStudy/CSharp3.0/8-PartialMethods.cs
@@ -0,0 +1,45 @@
+using System;
+
+namespace CSharpStudy.CSharp3_0
+{
+    public class PartialMethodsTest
+    {
+        public static void ExecuteExample()
+        {
+            var client = new Client();
+            client.ExternedMethod(19);
+        }        
+    }
+
+    internal partial class Client {
+
+        //Partial methods must have a void return type
+        //partial bool CalculateCredit(int id);
+
+        //A partial method cannot have access modifiers or the 
+        //virtual, abstract, override, new, sealed, or extern modifiers
+        //public partial void CalculateCredit(int id);
+
+        //No defining declaration found for implementing declaration of partial method
+        //partial void CalculateCredit(int id) {}
+
+        public void ExternedMethod(int id) {
+            Console.WriteLine("Calculate credit not called!");
+            CalculateCredit(id);
+            Console.WriteLine("Calculate credit called!");
+        }
+
+        //partial void CalculateCredit(int id);
+    }
+
+    internal partial class Client {
+
+        //A partial method may not have multiple defining declarations
+        // partial void CalculateCredit(int id);
+
+        //this is allowed
+        partial void CalculateCredit(int id){
+            Console.WriteLine("Calculate credit");
+        }
+    }
+}
