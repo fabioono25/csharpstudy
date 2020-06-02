e52098c6b0ad59012c18cbfd70ce2e7cd7b07e4e
Fabio Ono Tavoloni <fabioono25@gmail.com>
2020-06-02 14:43:39 +0100

    expression bodied

--- CSharpStudy/CSharp6.0/5-ExpressionBodied.cs
+++ CSharpStudy/CSharp6.0/5-ExpressionBodied.cs
@@ -1,9 +1,24 @@
+using static System.Console;
+
 namespace CSharpStudy.CSharp6
 {
     public class ExpressionBodied
     {
         public static void ExecuteExample()
         {
+            var car = new Car();
+
+            car.PrintDescriptionComplete();
+            WriteLine(car.ReturnDescriptionComplete());
         }
+    }
+
+    internal class Car {
+        public string Brand { get; set; }
+        public int Year { get; set; }
+        private string DescriptionComplete => $"{Brand} {Year}";
+
+        public void PrintDescriptionComplete() => WriteLine(DescriptionComplete);
+        public string ReturnDescriptionComplete() => DescriptionComplete;
     }
 }
