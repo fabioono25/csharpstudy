041d4cba742e9c5477c4a9a4213763e2bc1ddc5d
Fabio Ono Tavoloni <fabioono25@gmail.com>
2020-06-23 12:06:15 +0100

    expression bodied + out variables

---
+++ CSharpStudy/CSharp7.0/2-ExpressioBodied.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace CSharpStudy.CSharp7
+{
+    public class ExpressionBodied
+    {
+        // Expression-bodied constructor
+        public ExpressionBodied(string label) => this.Label = label;
+
+        // Expression-bodied finalizer
+        ~ExpressionBodied() => Console.Error.WriteLine("Finalized!");
+
+        private string label;
+
+        // Expression-bodied get / set accessors.
+        public string Label
+        {
+            get => label;
+            set => this.label = value ?? "Default label";
+        }
+    }
+}
