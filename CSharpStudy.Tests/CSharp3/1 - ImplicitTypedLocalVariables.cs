32a48c37a7463b9c9ba5fc7e9f0933b33149b2b8
fabioono25 <fabioono25@gmail.com>
2023-07-14 17:45:44 +0200

    implicitTypedLocalVar + ObjectCollectionInitializers

--- CSharpStudy.Tests/CSharp3/1 - ImplicitTypedLocalVariables.cs
+++ CSharpStudy.Tests/CSharp3/1 - ImplicitTypedLocalVariables.cs
@@ -1,14 +1,29 @@
 namespace CSharpStudy.Tests.CSharp3
 {
   /**
-  * 
+  * Implicit type local variables (var) allows us to declare a variable without specifying its type.
+  The compiler infers the type of the variable based on the assigned value.
+  https://learn.microsoft.com/en-us/dotnet/csharp/programming-guide/classes-and-structs/implicitly-typed-local-variables
   **/
   public class ImplicitTypedLocalVariablesTest
   {
     [Fact]
     public void Example()
     {
-      //TODO
+      var number = 10; //implicit typed
+      int number2 = 10;//explicit typed
+      Console.WriteLine(number.GetType().Name);
+      Console.WriteLine(number2.GetType().Name);
+
+      var name = "John";
+      Console.WriteLine(name.GetType().Name);
+
+      var x = new ClassTest(); //x is an instance of ClassTest
+      Console.WriteLine(x.GetType().Name);
+    }
+
+    protected class ClassTest
+    {
     }
   }
 }
--- CSharpStudy.Tests/CSharp3/2 - ObjectCollectionInitializers.cs
+++ CSharpStudy.Tests/CSharp3/2 - ObjectCollectionInitializers.cs
@@ -1,14 +1,46 @@
 namespace CSharpStudy.Tests.CSharp3
 {
   /**
-  * 
+  * Object and Collection Initializers provide a shorthand way to initialize objects and collections.
+  * https://docs.microsoft.com/en-us/dotnet/csharp/programming-guide/classes-and-structs/object-and-collection-initializers
   **/
   public class ObjectCollectionInitializersTest
   {
     [Fact]
-    public void Example()
+    public void ObjectInitializerTest()
     {
-      //TODO
+      var person1 = new Person();
+
+      var person2 = new Person("John");
+
+      var person3 = new Person
+      {
+        Name = "John",
+        Age = 30
+      };
+    }
+
+    [Fact]
+    public void CollectionInitializerTest()
+    {
+      var list = new List<int> { 1, 2, 3, 4, 5 };
+
+      foreach (var item in list) {
+        Console.WriteLine(item);
+      }
+    }
+
+    protected class Person
+    {
+      public string Name { get; set; }
+      public int Age { get; set; }
+
+      public Person() {
+      }
+
+      public Person(string name) {
+        Name = name;
+      }
     }
   }
 }
