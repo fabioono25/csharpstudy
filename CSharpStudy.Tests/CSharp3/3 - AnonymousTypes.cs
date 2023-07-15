83bec6bd1c74acbbef359550f220e095a39c0c10
fabioono25 <fabioono25@gmail.com>
2023-07-15 08:22:02 +0200

    anonymous + extension methods

--- CSharpStudy.Tests/CSharp3/3 - AnonymousTypes.cs
+++ CSharpStudy.Tests/CSharp3/3 - AnonymousTypes.cs
@@ -1,14 +1,36 @@
 namespace CSharpStudy.Tests.CSharp3
 {
   /**
-  * 
+  * anonymous types allow creating objects without writing a class definition for the data type.
+  they are useful for storing data temporarily.
+  https://docs.microsoft.com/en-us/dotnet/csharp/programming-guide/classes-and-structs/anonymous-types
   **/
   public class AnonymousTypesTest
   {
     [Fact]
     public void Example()
     {
-      //TODO
+      var person = new { Name = "John", Surname = "Nash", Age = 30 };
+
+      // person.Age = 23; //it's readonly property
+
+      Console.WriteLine(person.Name);
+    }
+
+    [Fact]
+    public void ExampleWithCollections()
+    {
+
+      //the objects inside of an array must have the same properties and property-types
+      var prodArray = new[] {
+                                new { Id = 1, Name = "Pen" },
+                                new { Id = 2, Name = "Pencil" }
+                            };
+
+      foreach (var item in prodArray)
+      {
+        Console.WriteLine(item.Name);
+      }
     }
   }
 }
--- CSharpStudy.Tests/CSharp3/4 - ExtensionMethods.cs
+++ CSharpStudy.Tests/CSharp3/4 - ExtensionMethods.cs
@@ -1,14 +1,67 @@
+using System.Globalization;
+
 namespace CSharpStudy.Tests.CSharp3
 {
   /**
-  * 
+  * extension methods enable you to "add" methods to existing types without creating a new derived type,
+  * recompiling, or otherwise modifying the original type.
+  * https://docs.microsoft.com/en-us/dotnet/csharp/programming-guide/classes-and-structs/extension-methods
   **/
   public class ExtensionMethodsTest
   {
     [Fact]
-    public void Example()
+    public void ExampleStringExtension()
     {
-      //TODO
+      var str = "JOHN NASH";
+      if (!str.IsEmpty())
+      {
+        Console.WriteLine(str.ToTitleCase());
+      }
+    }
+
+    [Fact]
+    public static void ExampleCustomExtension()
+    {
+      var customer = new Customer();
+      customer.Name = "John";
+
+      Console.WriteLine(customer.Name.AddHi(", are you good?")); //Hi John, are you good?
+
+      customer.ReverseName();
+      Console.WriteLine(customer.Name); //nhoJ
+    }
+  }
+  public static class StringExtensions
+  {
+    public static bool IsEmpty(this string str)
+    {
+      return string.IsNullOrEmpty(str);
+    }
+
+    public static string ToTitleCase(this string str)
+    {
+      return CultureInfo.CurrentCulture.TextInfo.ToTitleCase(str);
+    }
+  }
+
+  internal class Customer
+  {
+    public string Name { get; set; }
+  }
+
+  internal static class CustomerExtensions
+  {
+    public static string AddHi(this string name, string complement)
+    {
+      return string.Concat("Hi ", name, complement);
+    }
+
+    public static void ReverseName(this Customer customer)
+    {
+
+      var array = customer.Name.ToCharArray();
+      Array.Reverse(array);
+      customer.Name = new String(array);
     }
   }
 }
