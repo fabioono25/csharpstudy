97ee9572c780002aa18718c6297a19daf1b9ff9e
fabioono25 <fabioono25@gmail.com>
2023-07-16 16:17:28 +0200

    bootstrapping + dynamic types

---
+++ CSharpStudy.Tests/CSharp4/5 - COMInteropEnhancements.cs
@@ -0,0 +1,15 @@
+namespace CSharpStudy.Tests.CSharp4
+{
+  /**
+  * 
+  **/
+  public class COMInteropEnhancementsTest
+  {
+    [Fact]
+    public void Example()
+    {
+      //TODO
+      
+    }
+  }
+}
