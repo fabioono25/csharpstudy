5259d74c7ca8ca7f0d8a4f0f353ae5e8f7e1a355
fabioono25 <fabioono25@gmail.com>
2023-07-17 09:25:11 +0200

    array, starting

--- CSharpStudy.Tests/DataStructures/1 - Array.cs
+++ CSharpStudy.Tests/DataStructures/1 - Array.cs
@@ -1,14 +1,32 @@
 namespace CSharpStudy.Tests.DataStructures
 {
   /**
-  * 
+  * Arrays where introduced in version 1.0 of the .NET Framework and version 1.0 of the C# language.
+  * Arrays are a collection of elements of the same type. They are fixed-size and have a zero-based index.
+  * https://docs.microsoft.com/en-us/dotnet/csharp/programming-guide/arrays/
   **/
   public class ArrayTest
   {
+    // Single-Dimensional Array (1D Array): elements stored in a linear sequence
     [Fact]
-    public void Example()
+    public void SingleDimentionalArray()
     {
-      //TODO
+      int[] numbers = new int[5] { 1, 2, 3, 4, 5 };
+    }
+
+    // Multidimensional Array (2D Array): elements stored in multiple dimensions (rows and columns)
+    [Fact]
+    public void MultidimentionalArray()
+    {
+      int[,] matrix = new int[3, 3]
+      {
+          { 1, 2, 3 },
+          { 4, 5, 6 },
+          { 7, 8, 9 }
+      };
+
+      // print the middle item of the matrix
+      Console.WriteLine(matrix[1, 1]);
     }
   }
 }
