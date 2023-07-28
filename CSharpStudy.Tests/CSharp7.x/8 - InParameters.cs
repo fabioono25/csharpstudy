eedbc3c1a1dc15e790ad6f3949642796b941715f
fabioono25 <fabioono25@gmail.com>
2023-07-28 12:02:18 +0200

    More code for C# 7.x

---
+++ CSharpStudy.Tests/CSharp7.x/8 - InParameters.cs
@@ -0,0 +1,22 @@
+namespace CSharpStudy.Tests.CSharp7
+{
+  /**
+  * C# 7.3 - in parameters allows you to pass an argument to a method by reference rather than by value.
+  * https://docs.microsoft.com/en-us/dotnet/csharp/whats-new/csharp-7#in-parameters
+  **/
+  public class InParameters
+  {
+    [Fact]
+    public void Example()
+    {
+      int number = 42;
+      Process(in number); // Pass 'number' as a read-only in parameter
+    }
+
+    static void Process(in int value)
+    {
+      // value++; // Error: Cannot modify in parameter
+      Console.WriteLine(value);
+    }
+  }
+}
